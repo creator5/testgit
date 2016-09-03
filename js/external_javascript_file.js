1e9a21db74fd27f4924f744cb12c0208deaa199b
Test User <bwebmaster98@gmail.com>
2016-09-03 14:32:25 -0400

    I added external_javascript_file.html and external_javascript_file.js to tesgit directory

---
+++ js/external_javascript_file.js
@@ -0,0 +1,12 @@
+//Barry Abrams
+	//September 3, 2016
+	//Project 1-3 Javascript Book
+
+		document.write("<ul>");
+		document.write("<li>5.85% (24 Month Terms)</li>");
+		document.write("<li>6.25% (36 Month Terms)</li>");
+		document.write("<li>7.65% (48 Month Terms)</li>");
+		document.write("<ul>");
+		//Escape Character p.92 Javascript book
+		document.write("<p>\"Minimum down payment: 10%\"</p>");
+	 
